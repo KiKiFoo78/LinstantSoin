b6e8d5b08129f66b79df40e2f805d9bbf230f219
Arnaud <arnaud.hascoet@gmail.com>
2014-11-25 16:37:47 +0100

    side bar + plan + lecteur + whoswho faits en accueil

--- src/InstantSoin/ProductBundle/Controller/AccueilController.php
+++ src/InstantSoin/ProductBundle/Controller/AccueilController.php
@@ -22,4 +22,19 @@ class AccueilController extends Controller
         return $this->render('ProductBundle:Accueil:sidebar.html.twig');
     }
 
+    public function whoswhoAction() {
+
+        $search = $this->createFormBuilder()
+                                ->add('recherche', 'search', array('label' => '', 'attr' => array('class' => 'productSearch')))
+                                ->add('save', 'submit', array('label' => 'Rechercher','attr' => array('class' => 'productSearch')))
+                                ->getForm();
+       
+        return $this->render('ProductBundle:Accueil:whoswho.html.twig', array('search' => $search->createView()));
+    }
+
+
+    public function clairjoieAction() {
+       
+        return $this->redirect('http://www.clairjoie.com');
+    }
 }
